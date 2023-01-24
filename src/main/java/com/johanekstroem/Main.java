02353b2355a811e562da0af94c786fb0c8490fca
JohanEkström <je@viaduct.se>
2023-01-24 13:32:55 +0100

    Add boilerplate code for Javalin server

---
+++ src/main/java/com/johanekstroem/Main.java
@@ -0,0 +1,11 @@
+
+package com.johanekstroem;
+
+import io.javalin.Javalin;
+
+public class Main {
+  public static void main(String[] args) {
+    Javalin app = Javalin.create().start(7000);
+    app.get("/", ctx -> ctx.result("Hello World"));
+  }
+}
