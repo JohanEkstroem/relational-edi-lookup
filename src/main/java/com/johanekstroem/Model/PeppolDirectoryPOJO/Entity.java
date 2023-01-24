89a884a71d68d793cc600d10ebd6ba43bd0522b5
JohanEkström <je@viaduct.se>
2023-01-24 14:51:32 +0100

    Add PeppolDirectorPOJOs

---
+++ src/main/java/com/johanekstroem/Model/PeppolDirectoryPOJO/Entity.java
@@ -0,0 +1,40 @@
+package com.johanekstroem.Model.PeppolDirectoryPOJO;
+
+public class Entity {
+    private List<Name> name = null;
+    private String countryCode;
+    private List<Identifier> identifiers = null;
+    private String regDate;
+
+    public List<Name> getName() {
+        return name;
+    }
+
+    public void setName(List<Name> name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public List<Identifier> getIdentifiers() {
+        return identifiers;
+    }
+
+    public void setIdentifiers(List<Identifier> identifiers) {
+        this.identifiers = identifiers;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+}
