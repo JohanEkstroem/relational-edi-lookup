098d8fbec10fb22598e4110c35cbaa84afc67795
JohanEkström <je@viaduct.se>
2023-01-24 16:10:17 +0100

    Implement async request to Peppol Directory

--- src/main/java/com/johanekstroem/Controller/HandleRequest.java
+++ src/main/java/com/johanekstroem/Controller/HandleRequest.java
@@ -4,7 +4,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.johanekstroem.Model.PeppolDirectoryPOJO.PeppolDirectoryPOJO;
+import com.johanekstroem.Model.ResponseDTO.OrganizationDTO;
+import com.johanekstroem.Service.DTOService;
+import com.johanekstroem.Service.ExternalSources;
 
 import io.javalin.http.Context;
 
@@ -14,14 +25,32 @@ public class HandleRequest {
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://directory.peppol.eu/search/1.0/json?q=" + queryString + "+beautif=true"))
+                .uri(URI.create("https://directory.peppol.eu/search/1.0/json?q=" + queryString + "&beautify=true"))
                 .GET()
                 .build();
         return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
     }
 
     public static void wideSearchQuery(Context ctx) {
-        ctx.result("Hello World!");
+        List<OrganizationDTO> listOfOrganizationDTOs = new ArrayList<>();
+        var futurePeppolDirectory = peppolDirectoryLookup(ctx.queryString());
+
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(futurePeppolDirectory)
+                .thenAcceptAsync((Void) -> {
+                    ObjectMapper om = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+                            false);
+                    try {
+                        PeppolDirectoryPOJO resultFromPeppolDirectory = om.readValue(futurePeppolDirectory.get().body(),
+                                PeppolDirectoryPOJO.class);
+                        DTOService.fetchFromPeppolDirectory(listOfOrganizationDTOs, resultFromPeppolDirectory,
+                                ExternalSources.PeppolDirectory);
+
+                        ctx.json(listOfOrganizationDTOs);
+                    } catch (JsonProcessingException | InterruptedException | ExecutionException e) {
+                        ctx.result("Something went wrong!");
+                    }
+                });
+        ctx.future(() -> combinedFuture);
     }
 
 }
