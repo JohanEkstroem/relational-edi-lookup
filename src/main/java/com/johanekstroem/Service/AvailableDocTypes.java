2ec5ecef760d3490f4f5eaf3dd0bce63bcf10506
JohanEkström <je@viaduct.se>
2023-01-25 09:25:24 +0100

    Add AvailableDocTypes enum

---
+++ src/main/java/com/johanekstroem/Service/AvailableDocTypes.java
@@ -0,0 +1,12 @@
+package com.johanekstroem.Service;
+
+public enum AvailableDocTypes {
+    UNKNOWN,
+    INVOIC,
+    CREDITNOTE,
+    DELFOR,
+    ORDERRESPONSE,
+    CATALOGUE
+}
+// Todo, fill list from
+// https://docs.peppol.eu/edelivery/codelists/v7.4/Peppol%20Code%20Lists%20-%20Document%20types%20v7.4.html
