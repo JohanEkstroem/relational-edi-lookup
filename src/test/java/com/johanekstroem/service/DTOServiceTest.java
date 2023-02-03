ac3497e967e520c5c396b01dded9a22d6eeb0845
JohanEkström <je@viaduct.se>
2023-02-03 16:15:09 +0100

    Add unit test for getIdentifier method

---
+++ src/test/java/com/johanekstroem/service/DTOServiceTest.java
@@ -0,0 +1,52 @@
+package com.johanekstroem.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DTOServiceTest {
+
+
+    @Test
+    void givenUnknownIdentifierShouldReturnNull() {
+        // Arrange
+        DTOService service = new DTOService();
+
+        // Act
+        String result = service.getIdentifier("Jibberish");
+
+        // Assert
+        assertThat(result).isNull();
+
+    }
+
+    @ParameterizedTest(name = "given a {0} should give Identifier value or Null")
+    @MethodSource("getIdentifier")
+    void givenStringShouldReturnIdentifierValueOrNull(String identifier, String expectedResult) {
+        // Arrange
+        DTOService service = new DTOService();
+
+        // Act
+        var result = service.getIdentifier(identifier);
+
+        // Arrange
+        assertEquals(expectedResult, result);
+    }
+    private static Stream<Arguments> getIdentifier(){
+        return Stream.of(
+            Arguments.of("UNKNOWN", "UNKNOWN"),
+            Arguments.of("NAME", "NAME"),
+            Arguments.of("ORGNO", "ORGNO"),
+            Arguments.of("GLN", "GLN"),
+            Arguments.of("PEPPOLID", "PEPPOLID")
+        );
+    }
+
+}
